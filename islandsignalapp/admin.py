acf236f8fd84d72f1b3efeec1dcfa79323fbf949
ckntucker <ckntucker@gmail.com>
2022-08-26 08:50:08 -1000

    admin page works created media and static

--- islandsignalapp/admin.py
+++ islandsignalapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from islandsignalapp.models import Categories, SubCategories
+
+admin.site.register(Categories)
+admin.site.register(SubCategories)
