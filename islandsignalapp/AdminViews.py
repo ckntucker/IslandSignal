e23650444af58d3ccff3ce8586855c2f91409070
ckntucker <ckntucker@gmail.com>
2022-08-28 19:22:32 -1000

    error/success messages working

--- islandsignalapp/AdminViews.py
+++ islandsignalapp/AdminViews.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, CreateView, UpdateView,DetailView
 from islandsignalapp.models import Categories
+from django.contrib.messages.views import SuccessMessageMixin
+
 @login_required(login_url="/admin/")
 def admin_home(request):
    return render(request, "admin_templates/home.html")
@@ -10,8 +12,9 @@ class CategoriesListView(ListView):
     model=Categories
     template_name= "admin_templates/category_list.html"
 
-class CategoriesCreate(CreateView):
+class CategoriesCreate(SuccessMessageMixin,CreateView):
     model=Categories
+    success_message= "Category Added"
     fields="__all__"
     template_name="admin_templates/category_create.html"
     
