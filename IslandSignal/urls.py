72b393d59ddb6b97c70b6de6bda74ce3bca7b69b
ckntucker <ckntucker@gmail.com>
2022-08-29 06:57:06 -1000

    fixed migrations errors

--- IslandSignal/urls.py
+++ IslandSignal/urls.py
@@ -30,4 +30,8 @@ urlpatterns = [
     path('admin_home',AdminViews.admin_home,name="admin_home"),
     path('category_list',AdminViews.CategoriesListView.as_view(),name="category_list"),
     path('category_create',AdminViews.CategoriesCreate.as_view(),name="category_create"),
+    path('category_update/<slug:pk>',AdminViews.CategoriesUpdate.as_view(),name="category_update"),
+    path('sub_category_list',AdminViews.SubCategoriesListView.as_view(),name="sub_category_list"),
+    path('sub_category_create',AdminViews.SubCategoriesCreate.as_view(),name="sub_category_create"),
+    path('sub_category_update/<slug:pk>',AdminViews.SubCategoriesUpdate.as_view(),name="sub_category_update"),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- islandsignalapp/AdminViews.py
+++ islandsignalapp/AdminViews.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, CreateView, UpdateView,DetailView
-from islandsignalapp.models import Categories
+from islandsignalapp.models import Categories, SubCategories
 from django.contrib.messages.views import SuccessMessageMixin
 
 @login_required(login_url="/admin/")
@@ -17,4 +17,44 @@ class CategoriesCreate(SuccessMessageMixin,CreateView):
     success_message= "Category Added"
     fields="__all__"
     template_name="admin_templates/category_create.html"
-    
+
+class CategoriesUpdate(SuccessMessageMixin,UpdateView):
+    model=Categories
+    success_message= "Category Updated"
+    fields="__all__"
+    template_name="admin_templates/category_update.html"
+
+class SubCategoriesListView(ListView):
+    model=SubCategories
+    template_name= "admin_templates/sub_category_list.html"
+    paginate_by=3
+
+    def get_queryset(self):
+        filter_val=self.request.GET.get("filter","")
+        order_by=self.request.GET.get("orderby","id")
+        if filter_val!="":
+            cat=SubCategories.objects.filter(Q(title__contains=filter_val) | Q(description__contains=filter_val)).order_by(order_by)
+        else:
+            cat=SubCategories.objects.all().order_by(order_by)
+
+        return cat
+
+    def get_context_data(self,**kwargs):
+        context=super(SubCategoriesListView,self).get_context_data(**kwargs)
+        context["filter"]=self.request.GET.get("filter","")
+        context["orderby"]=self.request.GET.get("orderby","id")
+        context["all_table_fields"]=SubCategories._meta.get_fields()
+        return context
+
+class SubCategoriesCreate(SuccessMessageMixin,CreateView):
+    model=SubCategories
+    success_message= "Sub Category Added"
+    fields="__all__"
+    template_name="admin_templates/sub_category_create.html"
+
+class SubCategoriesUpdate(SuccessMessageMixin,UpdateView):
+    model=SubCategories
+    success_message= " Sub Category Updated"
+    fields="__all__"
+    template_name="admin_templates/sub_category_update.html"
+       
